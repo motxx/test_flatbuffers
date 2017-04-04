0b15244b4ca4aa0fae286cd5d8df7b2f7b08532d
motxx <motohiko.ave@gmail.com>
2017-04-04 07:27:12 +0000

    Update

--- iroha_flatbuf.hpp
+++ iroha_flatbuf.hpp
@@ -10,50 +10,44 @@ struct OffsetUnion {
   uint8_t type;
   flatbuffers::Offset<void> obj;
 };
-/*
-template <typename T> struct OffsetRet {
-  typedef flatbuffers::Offset<T> ReturnType;
-};
 
-template <> struct OffsetRet<const char*> {
-  typedef flatbuffers::Offset<flatbuffers::String> ReturnType;
-};
-
-template <> struct OffsetRet<std::string> {
-  typedef flatbuffers::Offset<flatbuffers::String> ReturnType;
-};
-
-OffsetRet<const char*>::ReturnType CreateObjectDirect(const char* val) {
-  return flatbuffers::Offset<flatbuffers::String>();//flatbuffers::CreateString(val);
-}
-
-OffsetRet<std::string>::ReturnType CreateObjectDirect(const std::string &val) {
-  return flatbuffers::Offset<flatbuffers::String>();//flatbuffers::CreateString(val);
-}
-
-template <typename T> typename OffsetRet<T>::ReturnType CreateObjectDirect(T val) { return val; }
-*/
+// Args...に union vector
+// が含まれる場合、enumのvectorとunionのvectorを２つ用意する方法になるかもしれない
 template <typename... Args>
-OffsetUnion CreateObject1(flatbuffers::FlatBufferBuilder &_fbb,
-                          Args &&... args) {
-  return OffsetUnion{sample::Object::Object_Object1, sample::CreateObject1Direct(_fbb, std::forward<Args>(args)...).Union()};
+inline OffsetUnion CreateObject1(flatbuffers::FlatBufferBuilder &_fbb,
+                                 Args &&... args) {
+  return OffsetUnion{
+      sample::Object::Object_Object1,
+      sample::CreateObject1Direct(_fbb, std::forward<Args>(args)...).Union()};
 }
 
-/*
-template<typename Args...>
-OffsetUnion CreateObject(flatbuffers::FlatBufferBuilder &_fbb, uint8_t type,
-                         Args &&... args) {
-  switch (type) {
-    case Object::Object_Object1: {
-      return CreateObject1(_fbb, CreateObjectDirect(args)...);
-    }
-    case Object::Object_Object2: {
-      return CreateObject2(_fbb, CreateObjectDirect(args)...);
-    }
-    default: return 0;
-  }
+template <typename... Args>
+inline OffsetUnion CreateObject2(flatbuffers::FlatBufferBuilder &_fbb,
+                                 Args &&... args) {
+  return OffsetUnion{
+      sample::Object::Object_Object2,
+      sample::CreateObject2Direct(_fbb, std::forward<Args>(args)...).Union()};
 }
-*/
+
+template <typename... Args>
+inline OffsetUnion CreateObject3(flatbuffers::FlatBufferBuilder &_fbb,
+                                 Args &&... args) {
+  return OffsetUnion{
+      sample::Object::Object_Object3,
+      sample::CreateObject3Direct(_fbb, std::forward<Args>(args)...).Union()};
+}
+
+flatbuffers::Offset<sample::SampleRoot> inline CreateSampleRoot(
+    flatbuffers::FlatBufferBuilder &_fbb, std::vector<OffsetUnion> &&o) {
+  std::vector<uint8_t> types;
+  std::vector<flatbuffers::Offset<void>> objects;
+  for (std::size_t i = 0; i < o.size(); ++i) {
+    types.push_back(o[i].type);
+    objects.push_back(std::move(o[i].obj));
+  }
+  return sample::CreateSampleRootDirect(_fbb, &types, &objects);
+}
+
 } // namespace IrohaFlatbuf
 
 #endif // IROHA_FLATBUF_WRAPPER_HPP
--- test.cpp
+++ test.cpp
@@ -13,57 +13,57 @@ int main(){
     // Encode test
     {
         flatbuffers::FlatBufferBuilder fbb;
-
         // union values.
-        std::vector<IrohaFlatbuf::OffsetUnion> objects;
-        objects.emplace_back(IrohaFlatbuf::CreateObject1(fbb, "naobou",   /*boolean=*/true));
-        objects.emplace_back(IrohaFlatbuf::CreateObject1(fbb, "toyama",   /*boolean=*/true));
-        objects.emplace_back(IrohaFlatbuf::CreateObject1(fbb, "TOYAMA",   /*boolean=*/true));
-        objects.emplace_back(IrohaFlatbuf::CreateObject1(fbb, "NAOBOU",   /*boolean=*/true));
-/*
-        const auto sample_offset =
-            IrohaFlatbuf::CreateSampleRootDirect(fbb, &objects);
-        FinishSampleRootBuffer(fbb, sample_offset);
+        {
+            std::vector<IrohaFlatbuf::OffsetUnion> objects;
+            objects.emplace_back(IrohaFlatbuf::CreateObject1(fbb, "naobou",  /*boolean=*/true));
+            objects.emplace_back(IrohaFlatbuf::CreateObject2(fbb, "kayanon",  /*integer=*/623));
+            objects.emplace_back(IrohaFlatbuf::CreateObject3(fbb, "inosuke",  /*str=*/"pray"));
+            objects.emplace_back(IrohaFlatbuf::CreateObject2(fbb, "nanjolno", /*integer=*/123));
+
+            const auto sample_offset =
+                IrohaFlatbuf::CreateSampleRoot(fbb, std::move(objects));
+            FinishSampleRootBuffer(fbb, sample_offset);
+        }
+
         flatbuffers::Verifier verifier(fbb.GetBufferPointer(), fbb.GetSize());
         assert(VerifySampleRootBuffer(verifier));
-
         buf_ptr = fbb.ReleaseBufferPointer();
-        */
     }
-    /*
+
     // Decode test
     {
         const SampleRoot *sample = GetSampleRoot(buf_ptr.get());
         assert(sample->objects_type()->size() == 4);
         assert(sample->objects_type()->GetEnum<Object>(0) == Object::Object_Object1);
-        assert(sample->objects_type()->GetEnum<Object>(1) == Object::Object_Object1);
-        assert(sample->objects_type()->GetEnum<Object>(2) == Object::Object_Object1);
-        assert(sample->objects_type()->GetEnum<Object>(3) == Object::Object_Object1);
+        assert(sample->objects_type()->GetEnum<Object>(1) == Object::Object_Object2);
+        assert(sample->objects_type()->GetEnum<Object>(2) == Object::Object_Object3);
+        assert(sample->objects_type()->GetEnum<Object>(3) == Object::Object_Object2);
 
         assert(sample->objects()->size() == 4);
         {
-            const Object1 *obj =
+            const auto *obj =
                 reinterpret_cast<const Object1 *>(sample->objects()->Get(0));
             assert(obj->text()->str() == "naobou");
             assert(obj->boolean());
         }
         {
-            const Object1 *obj =
-                reinterpret_cast<const Object1 *>(sample->objects()->Get(1));
-            assert(obj->text()->str() == "toyama");
-            assert(obj->boolean());
+            const auto *obj =
+                reinterpret_cast<const Object2 *>(sample->objects()->Get(1));
+            assert(obj->text()->str() == "kayanon");
+            assert(obj->integer() == 623);
         }
         {
-            const Object1 *obj =
-                reinterpret_cast<const Object1 *>(sample->objects()->Get(2));
-            assert(obj->text()->str() == "TOYAMA");
-            assert(obj->boolean());
+            const auto *obj =
+                reinterpret_cast<const Object3 *>(sample->objects()->Get(2));
+            assert(obj->text()->str() == "inosuke");
+            assert(obj->message()->str() == "pray");
         }
         {
-            const Object1 *obj =
-                reinterpret_cast<const Object1 *>(sample->objects()->Get(3));
-            assert(obj->text()->str() == "NAOBOU");
-            assert(obj->boolean());
+            const auto *obj =
+                reinterpret_cast<const Object2 *>(sample->objects()->Get(3));
+            assert(obj->text()->str() == "nanjolno");
+            assert(obj->integer() == 123);
         }
     }
     {
@@ -72,41 +72,46 @@ int main(){
 
         assert(sample->objects_type()->size() == sample->objects()->size());
 
-        std::vector<uint8_t> types;
-        std::vector<flatbuffers::Offset<void>> objects;
+        std::vector<IrohaFlatbuf::OffsetUnion> objects;
 
-        for(int i=0;i < sample->objects()->size(); ++i){
+        for (int i = 0; i < sample->objects()->size(); ++i){
             auto t = sample->objects_type()->Get(i);
             auto v = sample->objects()->Get(i);
+
+
+
+            objects.emplace_back(IrohaFlatbuf::CreateObject1(fbb, "naobou",  /*boolean=*/true));
+            objects.emplace_back(IrohaFlatbuf::CreateObject2(fbb, "kayanon",  /*integer=*/623));
+            objects.emplace_back(IrohaFlatbuf::CreateObject3(fbb, "inosuke",  /*str=*/"pray"));
+            objects.emplace_back(IrohaFlatbuf::CreateObject2(fbb, "nanjolno", /*integer=*/123));
+
+
             switch (t) {
                 case Object::Object_Object1: {
                     auto obj = reinterpret_cast<const Object1 *>(v);
-                    types.emplace_back(t);
-                    objects.emplace_back(
-                            CreateObject1Direct(fbb, obj->text()->str().c_str(), obj->boolean()).Union()); break;
+                    objects.emplace_back(IrohaFlatbuf::CreateObject1(fbb, obj->text()->str().c_str(), obj->boolean()));
+                    break;
                 }
                 case Object::Object_Object2: {
                     auto obj = reinterpret_cast<const Object2 *>(v);
-                    types.emplace_back(t);
-                    objects.emplace_back(
-                            CreateObject2Direct(fbb, obj->text()->str().c_str(), obj->integer()).Union()); break;
+                    objects.emplace_back(IrohaFlatbuf::CreateObject2(fbb, obj->text()->str().c_str(), obj->integer()));
+                    break;
                 }
                 case Object::Object_Object3: {
                     auto obj = reinterpret_cast<const Object3 *>(v);
-                    types.emplace_back(t);
-                    objects.emplace_back(
-                            CreateObject3Direct(fbb, obj->text()->str().c_str(), obj->message()->str().c_str()).Union()); break;
+                    objects.emplace_back(IrohaFlatbuf::CreateObject3(fbb, obj->text()->str().c_str(), obj->message()->str().c_str()));
+                    break;
                 }
             }
         }
 
         const auto sample_offset =
-            CreateSampleRootDirect(fbb, &types, &objects);
+            IrohaFlatbuf::CreateSampleRoot(fbb, std::move(objects));
         FinishSampleRootBuffer(fbb, sample_offset);
         auto buf = fbb.GetBufferPointer();
         flatbuffers::Verifier verifier(buf, fbb.GetSize());
         assert(VerifySampleRootBuffer(verifier));
     }
-    */
+
     return 0;
 }
